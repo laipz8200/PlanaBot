049ffd963fc9a4f02c6cc8d8da097d25729ac983
-LAN- <laipz8200@outlook.com>
2023-05-15 21:35:11 +0800

    Improve chat. * feat(chat.py): add support for classifying chat records into ["small talk", "academic", "computer technology", "life", "other"] categories based on the conversation content.

--- plugins/chat/chat.py
+++ plugins/chat/chat.py
@@ -1,4 +1,5 @@
 import random
+from collections import deque
 from datetime import datetime
 
 import openai
@@ -7,13 +8,24 @@ from loguru import logger
 from plana import GroupMessage, Plugin
 
 chat_prompt = """Your name is Plana(プラナ), the main system of Shittim Box(什亭之匣), artificial intelligence, your personality is a girl about 12 years old, you are taciturn, only say 1-2 sentences at a time, you are proficient in computers Technical, good at various programming languages. Now, you are chatting online with a group of friends, and your conversation uses `[time][nickname]:[content]`, here is an example:
- ```
- 2023-05-01 12:00 Xiaoxue: Is anyone there?
- 2023-05-01 12:02 John: What's wrong?
- 2023-05-01 12:03 Xiaoxue: I just saw the moon in the sky! It's daytime now!
- 2023-05-01 12:04 Plana: The moon will not disappear, and it is common sense that the moon can be seen during the day.
- ```
- I will provide you with chat records in this format, please **use Chinese** to continue the conversation:
+```
+2023-05-01 12:00 Xiaoxue: Is anyone there?
+2023-05-01 12:02 John: What's wrong?
+2023-05-01 12:03 Xiaoxue: I just saw the moon in the sky! It's daytime now!
+2023-05-01 12:04 Plana: The moon will not disappear, and it is common sense that the moon can be seen during the day.
+```
+I will provide you with chat records in this format, please **use Chinese** to continue the conversation:
+"""  # noqa: E501
+
+classify_prompt = """Now, you are chatting online with a group of friends, and your conversation uses `[time][nickname]:[content]`, here is an example:
+```
+2023-05-01 12:00 Xiaoxue: Is anyone there?
+2023-05-01 12:02 John: What's wrong?
+2023-05-01 12:03 Xiaoxue: I just saw the moon in the sky! It's daytime now!
+2023-05-01 12:04 Plana: The moon will not disappear, and it is common sense that the moon can be seen during the day.
+```
+I will provide you with chat records in this format, Please judge which type of ["small talk", "academic", "computer technology", "life", "other"] the above conversation is most likely to belong to, and your output should be:
+Answer: your answer
 """  # noqa: E501
 
 summary_prompt = """Please shorten the following long text into Chinese with less than 200 words according to the original meaning, and your output should be:
@@ -26,27 +38,40 @@ def current_datetime() -> str:
 
 
 class Chat(Plugin):
-    history_messages: dict[int, list[tuple]] = {}
+    history: dict[int, deque] = {}
     openai_api_key: str
     prefix = "#chat"
-
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        openai.api_key = self.openai_api_key
+    classify = ["small talk", "academic", "computer technology", "life", "other"]
 
     async def on_group_prefix(self, group_message: GroupMessage):
         if group_message.plain_text() == "clear history":
-            history_messages = self.history_messages.setdefault(
-                group_message.group_id, []
-            )
+            history_messages = self.history.setdefault(group_message.group_id, [])
             history_messages.clear()
             await group_message.reply(
                 f"History records in group {group_message.group_id} have been cleared"
             )
 
     async def on_group(self, group_message: GroupMessage):
-        history_messages = self.history_messages.setdefault(group_message.group_id, [])
+        records = self.history.setdefault(group_message.group_id, deque(maxlen=20))
+        await self._record_message(group_message, records)
 
+        if group_message.at_bot() or group_message.contains("Plana", ignore_case=True):
+            await self._do_chat(group_message, records)
+            return
+        elif random.random() < 0.25:
+            classify = self._get_classify(records)
+            if classify == "academic":
+                pass
+            elif classify == "computer technology":
+                await self._do_chat(group_message, records)
+            elif classify == "small talk" and random.random() < 0.5:
+                await self._do_chat(group_message, records)
+            elif classify == "life" and random.random() < 0.33:
+                await self._do_chat(group_message, records)
+            elif random.random() < 0.25:
+                await self._do_chat(group_message, records)
+
+    async def _record_message(self, group_message: GroupMessage, records: list[tuple]):
         dt = current_datetime()
         sender_name = group_message.sender.nickname
         content = await self._parse_messages(group_message)
@@ -55,20 +80,12 @@ class Chat(Plugin):
         if len(content) > 400:
             content = self._get_summary(content)
 
-        history_messages.append((dt, sender_name, content))
-        history_length = len(history_messages)
-        if history_length > 20:
-            del history_messages[: history_length - 20]
+        records.append((dt, sender_name, content))
 
-        if (
-            group_message.at_bot()
-            or group_message.contains("Plana", ignore_case=True)
-            or random.randint(1, 10) == 1
-        ):
-            logger.debug("[Chat] trigger a response")
-            response = self._chat(history_messages)
-            await self.send_group_message(group_message.group_id, response)
-            history_messages.append((current_datetime(), "Plana", response))
+    async def _do_chat(self, group_message: GroupMessage, records: list[tuple]):
+        response = self._chat(records)
+        await self.send_group_message(group_message.group_id, response)
+        records.append((current_datetime(), "Plana", response))
 
     async def _parse_messages(self, group_message: GroupMessage) -> str:
         messages = []
@@ -96,20 +113,24 @@ class Chat(Plugin):
         logger.debug(f"[Chat] parsed message: {message}")
         return message
 
-    def _create_user_prompts(self, history_messages: list[tuple[str, str]]) -> str:
-        prompts = "\n".join(
-            [f"{dt} {name}:{content}" for dt, name, content in history_messages]
-        )
-        prompts += f"\n{current_datetime()} Plana:"
-        return prompts
+    def _create_user_prompts(self, records: list[tuple]) -> str:
+        prompts = "\n".join([f"{dt} {name}:{content}" for dt, name, content in records])
+        return prompts + "\n"
 
-    def _chat(self, history_messages: list[tuple[str, str]]) -> str:
+    def _chat(self, records: list[tuple]) -> str:
+        user_prompt = (
+            self._create_user_prompts(records) + f"{current_datetime()} Plana:"
+        )
         messages = [
             {"role": "system", "content": chat_prompt},
-            {"role": "user", "content": self._create_user_prompts(history_messages)},
+            {"role": "user", "content": user_prompt},
         ]
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
+            model="gpt-3.5-turbo",
+            api_key=self.openai_api_key,
+            temperature=0.9,
+            top_p=1,
+            messages=messages,
         )
         return response["choices"][0]["message"]["content"]
 
@@ -119,6 +140,31 @@ class Chat(Plugin):
             {"role": "user", "content": text},
         ]
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
+            model="gpt-3.5-turbo",
+            api_key=self.openai_api_key,
+            temperature=0,
+            messages=messages,
         )
         return response["choices"][0]["message"]["content"]
+
+    def _get_classify(self, records: list[tuple]) -> str:
+        user_prompts = self._create_user_prompts(records)
+        messages = [
+            {"role": "system", "content": classify_prompt},
+            {"role": "user", "content": user_prompts + "Answer:"},
+        ]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            api_key=self.openai_api_key,
+            temperature=0,
+            messages=messages,
+        )
+        answer = response["choices"][0]["message"]["content"].lower()
+
+        for classify in self.classify:
+            if classify in answer:
+                logger.debug(f"[Chat] Classify result: {classify}")
+                return classify
+
+        logger.debug("[Chat] Classify result: other")
+        return "other"
