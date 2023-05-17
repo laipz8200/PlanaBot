7da1fe64cf87f0743591cd3295f96d1af78269b6
-LAN- <laipz8200@outlook.com>
2023-05-18 01:51:02 +0800

    Update chat.
    * style(format.sh): add black formatting to the entire project
    * chore(format.sh): add plugins directory to autoflake and isort commands
    * refactor(chat.py): remove unused imports and simplify message handling in on_group_prefix method

--- format.sh
+++ format.sh
@@ -1,3 +1,5 @@
 autoflake --remove-all-unused-imports -r --exclude __init__.py --in-place plana
-black plana
+autoflake --remove-all-unused-imports -r --exclude __init__.py --in-place plugins
 isort --profile black plana
+isort --profile black plugins
+black .
--- plugins/chat/chat.py
+++ plugins/chat/chat.py
@@ -1,11 +1,9 @@
-import json
 import openai
 from loguru import logger
 
 from plana import GroupMessage, Plugin
-from plana.messages import Message
+
 from .utils import get_completion
-from .prompts import chat_prompt
 
 
 class Chat(Plugin):
@@ -17,23 +15,8 @@ class Chat(Plugin):
         openai.api_key = self.openai_api_key
 
     async def on_group_prefix(self, group_message: GroupMessage) -> None:
-        group_messages = await self.get_group_msg_history(group_message.group_id)
-        history_list = [
-            f"{message.user_id}: {message.remove_prefix(self.prefix).plain_text()}"
-            for message in group_messages
-        ]
-        prompt = chat_prompt.format(history_list="\n".join(history_list))
+        prompt = group_message.plain_text()
         logger.debug(f"[Chat] {prompt=}")
         response = await get_completion(prompt)
         logger.debug(f"[Chat] {response=}")
-        try:
-            response_json = json.loads(response)
-            user_id = response_json["mention_user_id"]
-            content = response_json["content"]
-            message = Message()
-            message.add_at(user_id)
-            message.add_text(content)
-            await self.send_group_message(group_message.group_id, message)
-        except Exception as e:
-            logger.error(f"Failed to parse response: {e}")
-            await group_message.reply("Sorry, please try again later.")
+        await group_message.reply(response)
