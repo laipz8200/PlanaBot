25d1e6a7e87f05cb1bc801cff62d967597df18a6
-LAN- <laipz8200@outlook.com>
2023-05-17 23:10:27 +0800

    Run mikan at 18-02.
    * feat(mikan.py): add pydantic AnimeItem model to represent anime items in rss feed
    * refactor(mikan.py): refactor check_update method to use pydantic AnimeItem model
    * fix(mikan.py): fix check_update method to handle rss items without title or link attributes

--- plugins/mikan/mikan.py
+++ plugins/mikan/mikan.py
@@ -4,23 +4,29 @@ import xml.etree.ElementTree as ET
 import httpx
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from loguru import logger
+from pydantic import BaseModel
 
 from plana import Plugin
+
+
+class AnimeItem(BaseModel):
+    title: str
+    link: str
 
 
 class MikanAnime(Plugin):
     rss_url: str
     previous_records: list[str] = []
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         scheduler = AsyncIOScheduler()
-        scheduler.add_job(self.check_update, "cron", hour="18-23")
+        scheduler.add_job(self.check_update, "cron", hour="0-2,18-23")
         scheduler.start()
 
-    async def check_update(self):
-        logger.info("[MikanAnime] Start check update")
+    async def check_update(self) -> None:
+        logger.debug("[MikanAnime] Start check update")
         async with httpx.AsyncClient() as client:
             try:
                 response = await client.get(self.rss_url, timeout=3)
@@ -28,25 +34,31 @@ class MikanAnime(Plugin):
                 logger.error(f"[Mikan] Failed to fetch rss: {e}")
                 return
 
-        rss = ET.fromstring(response.text)
-        items = rss.findall("./channel/item")
-        rs = [
-            {"title": item.find("title").text, "link": item.find("link").text}
-            for item in filter(
-                lambda item: item.find("title").text not in self.previous_records,
-                items,
-            )
-        ]
-        if not rs:
-            return
-        message = "\n".join(
-            ["你订阅的番剧更新啦!!!"] + [f'{i["title"]}\n详情: {i["link"]}' for i in rs]
+        rss: ET.Element = ET.fromstring(response.text)
+        rss_items = filter(
+            lambda item: item.find("title") and item.find("link"),
+            rss.findall("./channel/item"),
         )
-        if self.previous_records:
-            asyncio.gather(
-                *[
-                    self.send_group_message(gid, message)
-                    for gid in self.config.allowed_groups
-                ]
-            )
-        self.previous_records = [r["title"] for r in rs]
+
+        anime_items: list[AnimeItem] = [
+            AnimeItem(title=i.find("title").text, link=i.find("link").text)  # type: ignore  # noqa: E501
+            for i in rss_items
+        ]
+
+        if not anime_items:
+            return
+
+        message_list: list[str] = [
+            f"{i.title}\n{i.link}"
+            for i in filter(lambda i: i.title not in self.previous_records, anime_items)
+        ]
+        message_list.insert(0, "老师, 你订阅的番剧更新了:")
+        message: str = "\n".join(message_list)
+
+        asyncio.gather(
+            *[
+                self.send_group_message(gid, message)
+                for gid in self.config.allowed_groups
+            ]
+        )
+        self.previous_records = [i.title for i in anime_items]
