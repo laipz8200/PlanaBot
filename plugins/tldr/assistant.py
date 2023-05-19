6034d1e121e661abc0d549020d6c28fbda00f026
-LAN- <laipz8200@outlook.com>
2023-05-20 04:09:48 +0800

    * refactor(assistant.py): remove headless option from playwright launch method in _fetch method of Assistant class

--- plugins/tldr/assistant.py
+++ plugins/tldr/assistant.py
@@ -61,7 +61,7 @@ class Assistant:
     async def _fetch(self, url) -> str:
         logger.info(f"Fetching {url}")
         async with async_playwright() as p:
-            browser = await p.chromium.launch(headless=False)
+            browser = await p.chromium.launch()
             page = await browser.new_page()
             await page.route("*", filter_type)
             await page.goto(url)
