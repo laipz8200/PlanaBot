2eadd983b696d09e2a42b0301a71e9eef0137ce7
-LAN- <laipz8200@outlook.com>
2023-06-20 02:26:43 +0800

    Set agent in chat plugin.

--- plugins/chat/chat.py
+++ plugins/chat/chat.py
@@ -1,19 +1,32 @@
+import typing
+
 import openai
-from loguru import logger
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.chat_models import ChatOpenAI
 
 from plana import Plugin
 from plana.messages import BaseMessage, GroupMessage, PrivateMessage
-
-from .utils import get_completion
 
 
 class Chat(Plugin):
     prefix: str = "#chat"
     openai_api_key: str = ""
+    agent: typing.Any = None
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         openai.api_key = self.openai_api_key
+        llm = ChatOpenAI(
+            model="gpt-3.5-turbo-16k",
+            temperature=0.0,
+            openai_api_key=self.openai_api_key,
+        )  # type: ignore
+        tools = load_tools(
+            ["python_repl", "requests", "llm-math", "ddg-search"], llm=llm
+        )
+        self.agent = initialize_agent(
+            tools, llm, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+        )
 
     async def on_private_prefix(self, message: PrivateMessage) -> None:
         await self._chat(message)
@@ -23,7 +36,8 @@ class Chat(Plugin):
 
     async def _chat(self, message: BaseMessage) -> None:
         prompt = message.plain_text()
-        logger.debug(f"[Chat] {prompt=}")
-        response = await get_completion(prompt)
-        logger.debug(f"[Chat] {response=}")
-        await message.reply(response)
+        return self.agent.run(prompt)
+        # logger.debug(f"[Chat] {prompt=}")
+        # response = await get_completion(prompt)
+        # logger.debug(f"[Chat] {response=}")
+        # await message.reply(response)
